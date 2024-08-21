ecb013ce1855e2fb3a434c4448074a77ad0a6496
Zac Vaughn <zacwilliamvaughn@gmail.com>
2024-08-21 18:07:02 +0800

    updated theme changer

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const hamburger = document.getElementById('hamburger');
+const title = document.getElementById('header-title');
 
 hamburger.addEventListener('click', () => {
     document.body.classList.toggle('hamburger-active');
+});
+
+title.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
 });
